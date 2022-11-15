212b1f0de5f0e9ed79c1358e5e1e6a375a434104
yeojinha <hayeojin4966@gmail.com>
2022-11-15 21:02:44 +0900

    Add #2 - error code, exception, api responses
    프로젝트에서 사용할 에러코드의 예외, 공통응답 객체 설계 , 직접 정의한 예외를 사용함으로써
    프로젝트에서 예상한 문제와 예상치 못한 문제를 분리하고자 한다.

---
+++ src/main/java/com/example/getinline/dto/ApiDataResponse.java
@@ -0,0 +1,28 @@
+package com.example.getinline.dto;
+
+import com.example.getinline.constant.ErrorCode;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class ApiDataResponse<T> extends ApiErrorResponse {
+
+    private final T data;
+
+    private ApiDataResponse(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
+        this.data = data;
+    }
+
+    public static <T> ApiDataResponse<T> of(T data) {
+        return new ApiDataResponse<>(data);
+    }
+
+    public static <T> ApiDataResponse<T> empty() {
+        return new ApiDataResponse<>(null);
+    }
+
+}
